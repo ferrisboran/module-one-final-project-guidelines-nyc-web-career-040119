b02d83e89a841cf985d11b6012e8943c9ac79bc2
Edgar Ong <edgar@minus.com>
2019-04-16 09:48:29 -0400

    Add details in Alien and Mortydex class

--- lib/aliens.rb
+++ lib/aliens.rb
@@ -1,3 +1,10 @@
+require 'json'
+require 'rest-client'
+require 'pry'
+
 class Alien < ActiveRecord::Base
 
+  # name, status (alive/dead), species, planet_id, points (based on length of name)
+
+  # cannot meet original Rick Sanchez and Morty Smith
 end
--- lib/mortydex.rb
+++ lib/mortydex.rb
@@ -1,3 +1,16 @@
 class Mortydex < ActiveRecord::Base
 
+  # CLASS METHODS
+  def self.total_aliens_collected
+    # can be used to display list of aliens
+  end
+
+  def self.total_planets_visited
+    # total planets will be based on aliens planet_id
+    # needs to return unique
+  end
+
+  def self.collect
+    # saves the alien to mortydex
+  end
 end
